147d123b2046d84420af314d8a37f12c1c888745
przemek <pluszkiewicz2gmail.com>
2020-03-29 13:04:08 +0200

    Initial commit

---
+++ src/main/java/com/pluszkie/herokutest/HerokuTestApplication.java
@@ -0,0 +1,24 @@
+package com.pluszkie.herokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HerokuTestApplication {
+
+	@GetMapping("/test")
+	@ResponseBody
+	String home() {
+		return "Dzień Dobry Bardzo";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluszkie/herokutest/HerokuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluszkie.herokutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
